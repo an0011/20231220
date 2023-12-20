c2ee2609c0e841ff1bfb155c45515abb8951edda
le402n18\uer <fsps5212a@yahoo.com.tw>
2023-12-20 16:55:24 +0800

    練習三

--- ui.R
+++ ui.R
@@ -2,17 +2,20 @@ library(shiny)
 
 shinyUI(pageWithSidebar(
   
-  headerPanel("Ploting your data"), 
+  headerPanel("Linear regression for two continuous variables."), 
   
   sidebarPanel(
     fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
     helpText("Note: you only can upload the .txt file."),
+    uiOutput("choose_columns1"),   #這裡是關鍵
+    uiOutput("choose_columns2"),   #這裡是關鍵
+    radioButtons("method", "What is the method to analysis?", choices = c("Pearson correlation" = "pearson", "Spearman correlation" = "spearman")),
     radioButtons("Color", "Select the color of histogram:", choices = c("Red" = "red", "Blue" = "blue", "Green" = "green"))
   ),
   
   mainPanel(
-    plotOutput("scatterPlot"),
-    downloadButton("download", label = "Download plot", class = NULL)
+    verbatimTextOutput("summary"),
+    plotOutput("plot",width = "500px", height = "500px")
   )  
   
 ))
